6fe30e6c1f53d6c2fed127853c970cfec882a260
anggape <anggape.dev@gmail.com>
2022-04-27 09:29:35 +0700

    source before sync

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,8 +4,9 @@ local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
+
 vim.api.nvim_create_autocmd('BufWritePost', {
-    command = 'PackerSync',
+    command = 'source <afile> | PackerSync',
     group = packer_group,
     pattern = 'plugins.lua'
 })
