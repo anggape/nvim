b5230258d86ee9fb99e4e5822af1aa656964f12c
Angga Permana <anggape.dev@gmail.com>
2026-04-24 10:49:15 +0700

    fix pum related issue
    - map `<CR>` to `<C-y>` when pum is visible
    - use `<Tab>` and `<S-Tab>` for selecting pum items

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,3 +12,23 @@ Map.ni('<M-w>', vim.cmd.bdelete)
 Map.ni('<M-c>', vim.cmd.bnext)
 Map.ni('<M-C>', vim.cmd.bprev)
 Map.ni('<M-s>', vim.cmd.write)
+
+local function smart_jump(lhs, direction)
+  local function rhs()
+    if vim.fn.pumvisible() == 1 then
+      return string.format('<C-%s>', direction == 1 and 'n' or 'p')
+    elseif vim.snippet.active({ direction = direction }) then
+      return string.format('<Cmd>lua vim.snippet.jump(%d)<CR>', direction)
+    end
+    return lhs
+  end
+  return lhs, rhs
+end
+
+Map:with({ expr = true }, function()
+  Map.i(smart_jump('<Tab>', 1))
+  Map.i(smart_jump('<S-Tab>', -1))
+  Map.i('<CR>', function()
+    return vim.fn.pumvisible() == 1 and '<C-y>' or '<CR>'
+  end)
+end)
