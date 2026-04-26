248bb51df22693561ab431093b79f54b8260b57c
Angga Permana <anggape.dev@gmail.com>
2026-04-26 20:44:20 +0700

    set diagnostic signs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -37,4 +37,12 @@ vim.opt.completeopt = {
 vim.diagnostic.config({
   update_in_insert = true,
   virtual_text = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = ' ',
+      [vim.diagnostic.severity.WARN] = ' ',
+      [vim.diagnostic.severity.HINT] = ' ',
+      [vim.diagnostic.severity.INFO] = ' ',
+    },
+  },
 })
