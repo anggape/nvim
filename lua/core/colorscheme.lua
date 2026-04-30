46b35eef43a3a14b328cf31a82a05d2809fcb1bb
Angga Permana <anggape.dev@gmail.com>
2026-04-30 22:44:30 +0700

    better completion style

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,2 +1,73 @@
 vim.cmd.colorscheme('catppuccin')
-vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'NormalFloat' })
+
+local colors = {
+  rosewater = '#f5e0dc',
+  flamingo = '#f2cdcd',
+  pink = '#f5c2e7',
+  mauve = '#cba6f7',
+  red = '#f38ba8',
+  maroon = '#eba0ac',
+  peach = '#fab387',
+  yellow = '#f9e2af',
+  green = '#a6e3a1',
+  teal = '#94e2d5',
+  sky = '#89dceb',
+  sapphire = '#74c7ec',
+  blue = '#89b4fa',
+  lavender = '#b4befe',
+  text = '#cdd6f4',
+  subtext1 = '#bac2de',
+  subtext0 = '#a6adc8',
+  overlay2 = '#9399b2',
+  overlay1 = '#7f849c',
+  overlay0 = '#6c7086',
+  surface2 = '#585b70',
+  surface1 = '#45475a',
+  surface0 = '#313244',
+  base = '#1e1e2e',
+  mantle = '#181825',
+  crust = '#11111b',
+}
+
+--- @type table<string, vim.api.keyset.highlight>
+local kinds = {
+  Text = { fg = colors.green },
+  Method = { fg = colors.blue },
+  Function = { fg = colors.blue },
+  Constructor = { fg = colors.blue },
+  Field = { fg = colors.green },
+  Variable = { fg = colors.flamingo },
+  Class = { fg = colors.yellow },
+  Interface = { fg = colors.yellow },
+  Module = { fg = colors.blue },
+  Property = { fg = colors.blue },
+  Unit = { fg = colors.green },
+  Value = { fg = colors.peach },
+  Enum = { fg = colors.yellow },
+  Keyword = { fg = colors.mauve },
+  Snippet = { fg = colors.flamingo },
+  Color = { fg = colors.red },
+  File = { fg = colors.blue },
+  Reference = { fg = colors.red },
+  Folder = { fg = colors.blue },
+  EnumMember = { fg = colors.teal },
+  Constant = { fg = colors.peach },
+  Struct = { fg = colors.blue },
+  Event = { fg = colors.blue },
+  Operator = { fg = colors.sky },
+  TypeParameter = { fg = colors.maroon },
+}
+
+--- @type table<string, vim.api.keyset.highlight>
+local highlights = {
+  FloatBorder = { link = 'NormalFloat' },
+}
+
+for key, value in pairs(kinds) do
+  highlights[key] = value
+  highlights['LspKind' .. key] = { link = key }
+end
+
+for key, value in pairs(highlights) do
+  vim.api.nvim_set_hl(0, key, value)
+end
--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -1,3 +1,4 @@
+local kind = vim.lsp.protocol.CompletionItemKind
 local Completion = {
   method = vim.lsp.protocol.Methods.textDocument_completion,
   trigger_chars = vim.split(
@@ -8,7 +9,45 @@ local Completion = {
       .. '!$*+,-./:;=?^_`@<>{[',
     ''
   ),
+  kinds = {
+    Text = '',
+    Method = '',
+    Function = '',
+    Constructor = '',
+    Field = '',
+    Variable = '',
+    Class = '',
+    Interface = '',
+    Module = '',
+    Property = '',
+    Unit = '',
+    Value = '',
+    Enum = '',
+    Keyword = '',
+    Snippet = '',
+    Color = '',
+    File = '',
+    Reference = '',
+    Folder = '',
+    EnumMember = '',
+    Constant = '',
+    Struct = '',
+    Event = '',
+    Operator = '',
+    TypeParameter = '',
+  },
 }
+
+function Completion._ellipsis(str, max_len)
+  local suffix = '...'
+  if #str <= max_len then
+    return str
+  end
+  if max_len <= #suffix then
+    return suffix:sub(1, max_len)
+  end
+  return str:sub(1, max_len - #suffix) .. suffix
+end
 
 --- @param client vim.lsp.Client
 --- @param buf integer
@@ -23,6 +62,19 @@ function Completion:on_attach(client, buf)
   end
   vim.lsp.completion.enable(true, client.id, buf, {
     autotrigger = true,
+    convert = function(item)
+      local label = string.format(
+        '%s%s',
+        item.label,
+        vim.tbl_get(item, 'labelDetails', 'detail') or ''
+      )
+      return {
+        abbr_hlgroup = 'LspKind' .. kind[item.kind],
+        abbr = self.kinds[kind[item.kind]],
+        kind = self._ellipsis(label, 30),
+        menu = client.name,
+      }
+    end,
   })
 end
 
