6f51323daca225756674672f06c9283db7e8122a
Angga Permana <anggape.dev@gmail.com>
2026-05-01 00:27:31 +0700

    another completion popup fix

--- lua/core/stupid_idea_remove_later.lua
+++ lua/core/stupid_idea_remove_later.lua
@@ -6,14 +6,36 @@
 local _complete_set = vim.api.nvim__complete_set
 vim.api.nvim__complete_set = function(...)
   local result = _complete_set(...)
-  if not result.winid then
-    return result
-  end
-
-  if vim.api.nvim_win_is_valid(result.winid) then
-    vim.api.nvim_win_set_config(result.winid, {
+  if result and result.winid and vim.api.nvim_win_is_valid(result.winid) then
+    pcall(vim.api.nvim_win_set_config, result.winid, {
       border = vim.o.winborder,
     })
   end
   return result
 end
+
+-- NOTE:
+--  another hack to fix popup window too wide after adding border.
+vim.api.nvim_create_autocmd('CompleteChanged', {
+  group = vim.api.nvim_create_augroup(
+    'stupid_idea_remove_later-complete-changed',
+    { clear = true }
+  ),
+  callback = function()
+    local info = vim.fn.complete_info({ 'selected' })
+    local win = info.preview_winid
+    if not win or not vim.api.nvim_win_is_valid(win) then
+      return
+    end
+
+    local width = vim.api.nvim_win_get_width(win)
+    local pos = vim.api.nvim_win_get_position(win)
+    local pumpos = vim.fn.pum_getpos()
+    local is_too_wide = (pos[2] - (pumpos.col + pumpos.width)) < 2
+
+    vim.api.nvim_win_set_config(win, {
+      border = vim.o.winborder,
+      width = is_too_wide and width - 2 or width,
+    })
+  end,
+})
